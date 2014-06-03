7971c4be15f01f24959a09dbe3a11177e67ebd46
Eric MSP Veith <eveith@wwweb-library.net>
2014-06-03 17:36:25 +0200

    ANN: Fixed iRPROP+ testcase

--- test/ann/RpropTrainingAlgorithmTest.cpp
+++ test/ann/RpropTrainingAlgorithmTest.cpp
@@ -48,11 +48,19 @@ void RpropTrainingAlgorithmTest::testTrainXOR()
     RpropTrainingAlgorithm trainingAlgorithm(network);
     trainingAlgorithm.train(&ts);
 
-    QCOMPARE(network->calculate({ 0.0, 1.0 }), { 1.0 });
-    QCOMPARE(network->calculate({ 1.0, 1.0 }), { 0.0 });
-    QCOMPARE(network->calculate({ 0.0, 0.0 }), { 0.0 });
-    QCOMPARE(network->calculate({ 1.0, 1.0 }), { 0.0 });
-    QCOMPARE(network->calculate({ 1.0, 0.0 }), { 1.0 });
+    ValueVector output;
+    output = network->calculate({ 1, 1 });
+    qDebug() << "(1, 1) =>" << output;
+    QCOMPARE(qRound(output[0]), 0);
+    output = network->calculate({ 1, 0 });
+    qDebug() << "(1, 0) =>" << output;
+    QCOMPARE(qRound(output[0]), 1);
+    output = network->calculate({ 0, 0 });
+    qDebug() << "(0, 0) =>" << output;
+    QCOMPARE(qRound(output[0]), 0);
+    output = network->calculate({ 0, 1 });
+    qDebug() << "(0, 1) =>" << output;
+    QCOMPARE(qRound(output[0]), 1);
 }
 
 
