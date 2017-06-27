b44a3bb7a14f98496711b75009df382fb0a07746
Eric MSP Veith <eveith@gnyu-linux.org>
2017-06-27 17:14:58 +0200

    Resurrected TraningSet test

--- test/TrainingSetTest.cpp
+++ test/TrainingSetTest.cpp
@@ -1,25 +1,17 @@
-#include <QtTest>
+#include <gtest/gtest.h>
 
 #include "NeuralNetwork.h"
 #include "TrainingSet.h"
 
-#include <gtest/gtest.h>
 #include "TrainingSetTest.h"
 
 
 using namespace Winzent::ANN;
 
 
-TrainingSetTest::TrainingSetTest(QObject *parent) :
-    QObject(parent)
-{
-}
-
-
 TEST(TrainingSetTest, testOutputRelevant)
 {
-    ASSERT_TRUE(
-            TrainingItem(Vector({ 1.0 }), Vector({ 1.0 })).outputRelevant());
+    ASSERT_TRUE(TrainingItem({ 1.0 }, { 1.0 }).outputRelevant());
     ASSERT_FALSE(TrainingItem(Vector()).outputRelevant());
 }
 
@@ -33,13 +25,10 @@ TEST(TrainingSetTest, testJsonSerialization)
             << TrainingItem({ 1.0, 1.0, 1.0 })
             << TrainingItem({ 2.0, 2.0, 2.0}, { 3.0, 3.0, 3.0 });
 
-    auto json = ts.toJSON();
-    qDebug() << json;
+    auto json = to_json(ts);
+    TrainingSet ts2{ from_json<TrainingSet>(json) };
 
-    TrainingSet ts2;
-    Winzent::deserialize(ts2, json);
-
-    ASSERT_EQ(ts.error(, ts2.error()));
-    ASSERT_EQ(ts.maxEpochs(, ts2.maxEpochs()));
-    ASSERT_EQ(ts.trainingItems.size(, ts2.trainingItems.size()));
+    ASSERT_EQ(ts.error(), ts2.error());
+    ASSERT_EQ(ts.maxEpochs(), ts2.maxEpochs());
+    ASSERT_EQ(ts.trainingItems.size(), ts2.trainingItems.size());
 }
--- test/TrainingSetTest.h
+++ test/TrainingSetTest.h
@@ -1,17 +1,6 @@
 #ifndef TRAININGSETTEST_H
 #define TRAININGSETTEST_H
 
-#include <QObject>
 
-class TrainingSetTest : public QObject
-{
-    Q_OBJECT
-public:
-    explicit TrainingSetTest(QObject *parent = 0);
-    
-private slots:
-    void testOutputRelevant();
-    void testJsonSerialization();
-};
 
 #endif // TRAININGSETTEST_H
